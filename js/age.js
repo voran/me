b111972336adbb244a6ec2a09e1ef61daf322490
Yavor Stoychev <stoychev.yavor@gmail.com>
2013-02-28 11:56:59 +0200

    updated age generation code

--- js/age.js
+++ js/age.js
@@ -1,2 +1,2 @@
-age = new Date()>getTime() - new Date("September 15, 1989 09:00:00").getTime();
+age = new Date().getTime() - new Date("September 15, 1989 09:00:00").getTime();
 $("#age").html("Age: " + age.getFullYear() + " years, " + (age.getMonth() + 1) + " months, " + age.getDate() + " days, " + age.getHours() + " hours");
