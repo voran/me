184f5e119d1bf339680f28cd9373f28848b94003
Yavor Stoychev <stoychev.yavor@gmail.com>
2013-02-28 12:09:45 +0200

    subtract 1970 from year (UNIX milliseconds)

--- js/age.js
+++ js/age.js
@@ -1,2 +1,4 @@
 age = new Date(new Date().getTime() - new Date(1989, 09, 15, 9).getTime());
-$("#age").html("Age: " + age.getFullYear() + " years, " + (age.getMonth() + 1) + " months, " + age.getDate() + " days, " + age.getHours() + " hours");
+
+
+$("#age").html("Age: " + (age.getFullYear() - new Date(0).getFullYear()) + " years, " + (age.getMonth() + 1) + " months, " + age.getDate() + " days, " + age.getHours() + " hours");
