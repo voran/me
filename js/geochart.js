88544e803ee5e5bc8ced62ca2c4867536b57b839
Yavor Stoychev <yavor.stoychev@transmetrics.eu>
2013-07-07 13:19:47 +0300

    added travel map

---
+++ js/geochart.js
@@ -0,0 +1,29 @@
+google.load('visualization', '1.1', {'packages': ['geochart']});
+google.setOnLoadCallback(drawVisualization);
+
+
+function drawVisualization() {
+  var data = google.visualization.arrayToDataTable([
+    ['Country', 'Time Factor'],
+    ['Austria', 1],
+    ['Germany', 3],
+    ['United States', 3],
+    ['Czech Republic', 1],
+    ['France', 1],
+    ['Netherlands', 1],
+    ['Norway', 1],
+    ['Greece', 1],
+    ['Bulgaria', 3],
+    ['Italy', 1],
+    ['Slovenia', 1],
+    ['Slovakia', 1],
+    ['Croatia', 1],
+    ['Belgium', 1],
+    ['Serbia', 1],
+    ['Hungary', 1],
+    ['Poland', 1]
+  ]);
+
+  var geochart = new google.visualization.GeoChart(document.getElementById('visualization'));
+  geochart.draw(data, {width: 1280, height: 720, colorAxis: {minValue: 1, maxValue: 2, colors: ['#4FBF43','#4381BF']}});
+}
