5a8476c17cd2c6ec3e8ef2bb2f3e615961c88fa9
Yavor Stoychev <yavor.stoychev@transmetrics.eu>
2013-12-25 01:55:30 -0800

    facelift

---
+++ js/blueimp-initializer.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+        $('.preview').click(function (event) {
+            event = event || window.event;
+            var target = event.target || event.srcElement,
+                link = target.src ? target.parentNode : target,
+                options = {index: link, event: event},
+                links = this.getElementsByTagName('a');
+            blueimp.Gallery(links, options);
+        });
+});
